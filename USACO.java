cc26cb87e11f1ce4188c43c2e738096fed022ac0
vivianhuynh91827 <vhuynh00@stuy.edu>
2019-03-07 10:54:41 -0500

    bronze problem solved

--- USACO.java
+++ USACO.java
@@ -5,10 +5,10 @@ public class USACO {
   public static int bronze(String filename) throws FileNotFoundException {
     File f = new File(filename);
     Scanner in = new Scanner(f);
-    int[] firstLine = new int[4];
+    int[] firstLine = new int[4]; //R, C, E, N
     String cur = "";
     int curInd = 0;
-    while (curInd < 4){
+    while (curInd < 4) {
       cur = in.next();
       firstLine[curInd] = Integer.parseInt(cur);
       curInd++;
@@ -33,10 +33,47 @@ public class USACO {
     //   test += directions[i][0] + " " + directions[i][1] + " " + directions[i][2] + "\n";
     // }
     // System.out.println(test);
-    return 0;
+
+    for (int i = 0 ; i < directions.length; i++) {
+      stomp(field, directions[i][0], directions[i][1], directions[i][2]);
+    }
+
+    // String testField = "";
+    // for (int r = 0; r < field.length; r++) {
+    //   for (int c = 0; c < field[0].length; c++) {
+    //     testField += field[r][c] + " ";
+    //   }
+    //   testField += "\n";
+    // }
+    // System.out.println(testField);
+    int sum = 0;
+    for (int r = 0; r < field.length; r ++) {
+      for (int c = 0; c < field[0].length; c++) {
+        field[r][c] = firstLine[2] - field[r][c];
+        if (field[r][c] <= 0) field[r][c] = 0;
+        sum += field[r][c];
+      }
+    }
+    return sum * 72 * 72;
   }
 
-  // private static void stomp(int[][] field, )
+  private static void stomp(int[][] field, int row, int col, int depth) {
+    int[][] grid = new int[3][3];
+    int highest = 0;
+    for (int r = 0; r < 3; r ++) {
+      for (int c = 0; c < 3; c++) {
+        grid[r][c] = field[row+r-1][col+c-1];
+        if (grid[r][c] > highest) highest = grid[r][c];
+      }
+    }
+    int lowest = highest - depth;
+    for (int r = 0; r < 3; r++) {
+      for (int c = 0; c < 3; c++) {
+        if (grid[r][c] > lowest) grid[r][c] = lowest;
+        field[row+r-1][col+c-1] = grid[r][c];
+      }
+    }
+  }
 
   public static int silver(String filename) throws FileNotFoundException {
     return 0;
@@ -44,7 +81,7 @@ public class USACO {
 
   public static void main(String[] args) {
     try {
-      bronze("makelake.1.in");
+      System.out.println(bronze("makelake.2.in"));
     }
     catch (FileNotFoundException e) {}
   }
